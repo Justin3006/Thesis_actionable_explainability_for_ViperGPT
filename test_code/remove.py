d600ce094f7939b906feec92f5fe1e52e277d76d
Justin3006 <75027185+Justin3006@users.noreply.github.com>
2025-06-25 20:13:43 +0200

    Prototype implementation for removing module from pre-prompt.

---
+++ test_code/remove.py
@@ -0,0 +1,158 @@
+
+def count_leading_whitespace(text:str) -> int:
+    count = 0
+    for char in text:
+        if char.isspace():
+            count += 1
+        else:
+            break
+    return count
+
+
+def remove_function_definition(code: str, function_name: str) -> str:
+    # Split the code into individual lines
+    lines = code.split('\n')
+    # Initialize a list to hold the lines of the updated code
+    updated_lines = []
+    # Flag to indicate whether we are inside the function to be removed
+    inside_function = False
+    # Flag to indicate whether we are inside Methods documentation
+    inside_methods = False
+    inside_definition = False
+    ident = 0
+
+    for line in lines:
+        # Check if the line contains the methods string
+        if line.strip().startswith('Methods'):
+            inside_methods = True
+            ident = count_leading_whitespace(line)
+        
+        # Check if the line contains a definition to remove
+        if inside_methods:
+            if line.strip().startswith(function_name):
+                inside_definition = True
+                continue
+            if inside_definition:
+                if count_leading_whitespace(line) > ident:
+                    continue
+                else:
+                    inside_definition = False
+            if line.strip().startswith('"""'): 
+                inside_methods = False
+
+        # Check if the line contains the function definition we want to remove
+        if line.strip().startswith(f'def {function_name}'):
+            inside_function = True
+            continue
+        # If we are inside the function, skip adding the line to the updated code
+        if inside_function:
+            if line.strip().startswith('return'):
+                # If we encounter an empty line, we might have reached the end of the function
+                inside_function = False
+            continue
+        # Add the line to the updated code
+        updated_lines.append(line)
+
+    # Join the updated lines back into a single string
+    return '\n'.join(updated_lines)
+
+
+# Example usage:
+code = '''
+class ImagePatch:
+    """A Python class containing a crop of an image centered around a particular object, as well as relevant information.
+    Attributes
+    ----------
+    cropped_image : array_like
+        An array-like of the cropped image taken from the original image.
+    left, lower, right, upper : int
+        An int describing the position of the (left/lower/right/upper) border of the crop's bounding box in the original image.
+
+    Methods
+    -------
+    find(object_name: str)->List[ImagePatch]
+        Returns a list of new ImagePatch objects containing crops of the image centered around any objects found in the
+        image matching the object_name.
+    exists(object_name: str)->bool
+        Returns True if the object specified by object_name is found in the image, and False otherwise.
+    """
+
+    def __init__(self, image, left: int = None, lower: int = None, right: int = None, upper: int = None):
+        """Initializes an ImagePatch object by cropping the image at the given coordinates and stores the coordinates as
+        attributes. If no coordinates are provided, the image is left unmodified, and the coordinates are set to the
+        dimensions of the image.
+        Parameters
+        -------
+        image : array_like
+            An array-like of the original image.
+        left, lower, right, upper : int
+            An int describing the position of the (left/lower/right/upper) border of the crop's bounding box in the original image.
+        """
+        if left is None and right is None and upper is None and lower is None:
+            self.cropped_image = image
+            self.left = 0
+            self.lower = 0
+            self.right = image.shape[2]  # width
+            self.upper = image.shape[1]  # height
+        else:
+            self.cropped_image = image[:, lower:upper, left:right]
+            self.left = left
+            self.upper = upper
+            self.right = right
+            self.lower = lower
+
+        self.width = self.cropped_image.shape[2]
+        self.height = self.cropped_image.shape[1]
+
+        self.horizontal_center = (self.left + self.right) / 2
+        self.vertical_center = (self.lower + self.upper) / 2
+
+    def find(self, object_name: str) -> List[ImagePatch]:
+        """Returns a list of ImagePatch objects matching object_name contained in the crop if any are found.
+        Otherwise, returns an empty list.
+        Parameters
+        ----------
+        object_name : str
+            the name of the object to be found
+
+        Returns
+        -------
+        List[ImagePatch]
+            a list of ImagePatch objects matching object_name contained in the crop
+
+        Examples
+        --------
+        >>> # return the foo
+        >>> def execute_command(image) -> List[ImagePatch]:
+        >>>     image_patch = ImagePatch(image)
+        >>>     foo_patches = image_patch.find("foo")
+        >>>     return foo_patches
+        """
+        return find_in_image(self.cropped_image, object_name)
+
+    def exists(self, object_name: str) -> bool:
+        """Returns True if the object specified by object_name is found in the image, and False otherwise.
+        Parameters
+        -------
+        object_name : str
+            A string describing the name of the object to be found in the image.
+
+        Examples
+        -------
+        >>> # Are there both foos and garply bars in the photo?
+        >>> def execute_command(image)->str:
+        >>>     image_patch = ImagePatch(image)
+        >>>     is_foo = image_patch.exists("foo")
+        >>>     is_garply_bar = image_patch.exists("garply bar")
+        >>>     return bool_to_yesno(is_foo and is_garply_bar)
+        """
+        return len(find_in_image(self.cropped_image, object_name)) > 0
+
+# Examples of how to use the API
+# INSERT_QUERY_HERE
+def execute_command(INSERT_TYPE_HERE):
+    '''
+
+function_name = "exists"
+updated_code = remove_function_definition(code, function_name)
+print(updated_code)
